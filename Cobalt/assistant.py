21c817cd97ae06eae9e02369fab2911f59968b17
vincent-bell <vincent@hypha.cc>
2022-06-16 20:45:19 +0100

    Updated assistant.py

--- Cobalt/assistant.py
+++ Cobalt/assistant.py
@@ -1,9 +1,11 @@
 import subprocess
+from json import dumps
 from .utils import parse_ljson
 from .utils import console
 
 
 ENCODING_FORMAT = "utf-8"
+
 
 class Assistant:
     def __init__(self):
@@ -16,30 +18,48 @@ class Assistant:
         for task in tasks:
             if task["status"] == "incomplete":
                 return task["id"], task["inputs"], task["outputs"]
-
+    
     def run_current_task(self):
-        cfile = f'task{self.current_task}.py' if self.current_task else None
-        if cfile:
-            if self.inputs:
-                test_count = 1
-                for each in self.inputs:
-                    ro = subprocess.run(["python", cfile, str(each)], capture_output=True)
-                    output = " ".join(ro.stdout.decode(ENCODING_FORMAT).split())
-                    console.print("  Test Case #{}".format(test_count))
-                    if str(output) == str(self.outputs[test_count - 1]):
-                        console.print("    Passed!")
-                    elif ro.stderr:
-                        console.print(ro.stderr.decode(ENCODING_FORMAT))
-                    else:
-                        console.print("    Failed...")
-                    test_count += 1
-            else:
-                ro = subprocess.run(["python", cfile], capture_output=True)
+        tester = Tester(self.current_task, self.inputs, self.outputs)
+        tester.run_full_test()
+
+
+class Tester:
+    def __init__(self, task_id: int = 0, inputs: list = None, outputs: list = []):
+        if task_id != 0: self.task_id = task_id
+        if inputs: self.inputs = inputs
+        self.outputs = outputs
+
+    def __testeq__(self, inv: any, outv: any):
+        inv = eval(str(inv))
+        outv = eval(str(outv))
+        if inv == outv:
+            return True
+        else:
+            return False
+
+    def run_full_test(self):
+        file = f'task{self.task_id}.py' if self.task_id else None
+        if self.inputs:
+            test_count = 1
+            passed = 0
+            for inv in self.inputs:
+                ro = subprocess.run(["python", file, str(inv)], capture_output=True)
                 output = " ".join(ro.stdout.decode(ENCODING_FORMAT).split())
-                console.print("  Testing {}".format(cfile))
-                if output == self.test_cases[0] and ro.returncode == 0:
+                console.print("  Test Case #{}".format(test_count))
+                if self.__testeq__(output, self.outputs[test_count - 1]):
                     console.print("    Passed!")
+                    passed += 1
                 elif ro.stderr:
                     console.print(ro.stderr.decode(ENCODING_FORMAT))
                 else:
                     console.print("    Failed...")
+                test_count += 1
+            if passed +1 == test_count:
+                self.update_task_status()
+
+    def update_task_status(self):
+        tasks = parse_ljson()
+        tasks[self.task_id - 1]["status"] = "completed"
+        with open("Cobalt/lesson.json", 'w') as f:
+            f.write(dumps(tasks, indent=4))
