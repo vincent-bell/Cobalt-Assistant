0f5e5a3ee99557371a3df163c19baceafcdb6513
vincent-bell <vincent@hypha.cc>
2022-06-16 18:49:31 +0100

    Merged tester and assistant

--- Cobalt/assistant.py
+++ Cobalt/assistant.py
@@ -1,29 +1,46 @@
+import subprocess
 from .tester import Tester
+from .utils.json_parser import parse_ljson
+from .utils.rich_console import console
 
+
+ENCODING_FORMAT = "utf-8"
 
 class Assistant:
-    def __init__(self, default_config_file="Cobalt/config.txt"):
-        self.config_file = default_config_file
-        self.current_task, self.test_cases, self.inputs = self.get_current_task()
+    def __init__(self):
+        self.current_task, self.inputs, self.outputs = self.get_current_task()
         if self.current_task == None:
-            print("There are no incomplete tasks left!")
+            console.print("There are no incomplete tasks left!")
 
     def get_current_task(self):
-        with open(self.config_file, 'r') as file:
-            data = list(map(lambda x: x.strip(), file.readlines()))
-        for i in range(len(data)):
-            if "TASK" in data[i] and "INCOMPLETE" in data[i+1]:
-                if "[" in data[i+1][11:]:
-                    r3 = eval(data[i+1][11:])
-                else:
-                    r3 = None
-                return int(data[i][5]), eval(data[i][8:]), r3
+        tasks = parse_ljson()
+        for task in tasks:
+            if task["status"] == "incomplete":
+                return task["id"], task["inputs"], task["outputs"]
 
     def run_current_task(self):
-        tester = Tester(f'task{self.current_task}.py', self.test_cases, self.inputs)
-        tester.run_test_cases()
-
-
-if __name__ == '__main__':
-    assistant = Assistant()
-    assistant.run_current_task()
+        cfile = f'task{self.current_task}.py' if self.current_task else None
+        if cfile:
+            if self.inputs:
+                test_count = 1
+                for each in self.inputs:
+                    ro = subprocess.run(["python", cfile, str(each)], capture_output=True)
+                    output = " ".join(ro.stdout.decode(ENCODING_FORMAT).split())
+                    console.print("  Test Case #{}".format(test_count))
+                    if str(output) == str(self.outputs[test_count - 1]):
+                        console.print("    Passed!")
+                    elif ro.stderr:
+                        console.print(ro.stderr.decode(ENCODING_FORMAT))
+                    else:
+                        console.print("    Failed...")
+                    test_count += 1
+            else:
+                ro = subprocess.run(["python", cfile], capture_output=True)
+                output = " ".join(ro.stdout.decode(ENCODING_FORMAT).split())
+                console.print("  Testing {}".format(cfile))
+                if output == self.test_cases[0] and ro.returncode == 0:
+                    console.print("    Passed!")
+                elif ro.stderr:
+                    console.print(ro.stderr.decode(ENCODING_FORMAT))
+                else:
+                    console.print("    Failed...")
